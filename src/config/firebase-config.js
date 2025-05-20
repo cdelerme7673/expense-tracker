4398f9f66255f7bfeb9058d9e3b7dabcd0a29975
CDelerme2 <delerme.biz@outlook.com>
2025-05-20 16:16:31 -0400

    fourth commit

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -6,19 +6,15 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// (These should be environment vars in prod -CD)
-const firebaseConfig = {
-  apiKey: "AIzaSyCd2hpQIr6WlAKHPfFvtj5-9zaLq2Qyfz0",
-  authDomain: "expense-tracker-88f88.firebaseapp.com",
-  projectId: "expense-tracker-88f88",
-  storageBucket: "expense-tracker-88f88.firebasestorage.app",
-  messagingSenderId: "971422880700",
-  appId: "1:971422880700:web:6cd86e0d9ac0a5b7d951b9",
-};
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+  apiKey: process.env.REACT_APP_FIRBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIRBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIRBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIRBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIRBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIRBASE_APP_ID,
+});
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
